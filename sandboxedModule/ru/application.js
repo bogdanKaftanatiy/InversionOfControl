004ecb811cc2717152a264c590d0c0bbf47fe5cf
bogan.kaftanatiy <bodia005@gmail.com>
2016-03-20 17:43:16 +0200

    second task

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -7,12 +7,21 @@ console.log('From application global context');
 
 module.exports = function() {
   // Вывод из контекста экспортируемой функции
+
   setTimeout(function(){
-  	console.log("Print something!!!!!=(")
-  }, 1000);
+  	console.log("Print use setTimeout.")
+  }, 3500);
   setInterval(function(){
-	console.log("Print something!!!!!=)")
+	console.log("Print use setInterval.")
   }, 1000);
-  
+
+  var util = require('util');
+  var obj = {
+  	a: 5,
+  	b: 6
+  }
+  obj.self = obj;
+  console.log(util.inspect(obj));
+
   console.log('From application exported function');
 };
