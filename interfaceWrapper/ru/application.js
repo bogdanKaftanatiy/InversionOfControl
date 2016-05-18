6b7b1f243ca96e519832c507c044e6ecae650b76
bogan.kaftanatiy <bodia005@gmail.com>
2016-05-18 18:01:35 +0300

    second lab

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -11,7 +11,20 @@
 
 
 var fileName = './README.md';
+var countIteration = 10;
 console.log('Application going to read ' + fileName);
-fs.readFile(fileName, function(err, src) {
-	console.log('File ' + fileName + ' size ' + src.length);
-});
+setTimeout(function f(){
+	countIteration--;
+
+	fs.readFile(fileName, function(err, src) {
+		console.log('File ' + fileName + ' size ' + src.length);
+	});
+
+	if (countIteration>0) {
+		setTimeout(f, 2500);
+	}
+}, 1000);
+
+setInterval(function(){
+	printInformation();
+}, 3000);
--- interfaceWrapper/ru/framework.js
+++ interfaceWrapper/ru/framework.js
@@ -8,28 +8,35 @@ var context = {
   module: {},
   console: console,
   // Помещаем ссылку на fs API в песочницу
-  fs: cloneInterface(fs),
+  fs: wrapAll(fs),
   // Оборачиваем функцию setTimeout в песочнице
-  setTimeout: function(callback, timeout) {
-    // Добавляем поведение при вызове setTimeout
-    console.log(
-      'Call: setTimeout, ' +
-      'callback function: ' + callback.name + ', ' +
-      'timeout: ' + timeout
-    );
-    setTimeout(function() {
-      // Добавляем поведение при срабатывании таймера
-      console.log('Event: setTimeout, before callback');
-      // Вызываем функцию пользователя на событии таймера
-      callback();
-      console.log('Event: setTimeout, after callback');
-    }, timeout);
-  }
+  // setTimeout: function(callback, timeout) {
+  //   // Добавляем поведение при вызове setTimeout
+  //   console.log(
+  //     'Call: setTimeout, ' +
+  //     'callback function: ' + callback.name + ', ' +
+  //     'timeout: ' + timeout
+  //   );
+  //   setTimeout(function() {
+  //     // Добавляем поведение при срабатывании таймера
+  //     console.log('Event: setTimeout, before callback');
+  //     // Вызываем функцию пользователя на событии таймера
+  //     callback();
+  //     console.log('Event: setTimeout, after callback');
+  //   }, timeout);
+  // }
+  setTimeout: setTimeout,
+  setInterval: setInterval,
+  printInformation: printInformation
 };
 
 // Преобразовываем хеш в контекст
 context.global = context;
 var sandbox = vm.createContext(context);
+var countCallFunction = 0;
+var coutCallBack = 0;
+var avgTime = 0;
+var totalSize = 0;
 
 // Читаем исходный код приложения из файла
 var fileName = './application.js';
@@ -42,10 +49,7 @@ fs.readFile(fileName, function(err, src) {
 function cloneInterface(anInterface) {
 	var clone = {};
     for (var key in anInterface) {
-    	/*if(anInterface[key].arguments[arguments.lenght - 1] == 'callback')
-    		clone[key] = wrapFunction(key, anInterface[key]);
-    	else*/
-    		clone[key] = anInterface[key];
+    	clone[key] = anInterface[key];
     }
     return clone;
 }
@@ -53,9 +57,41 @@ function cloneInterface(anInterface) {
 function wrapFunction(fnName, fn) {
     return function wrapper() {
       var args = [];
+      countCallFunction++;
       Array.prototype.push.apply(args, arguments);
+
       console.log('Call: ' + fnName);
-      console.dir(args);
-      return fn.apply(undefined, args);
+      //console.dir(args);
+
+      if(typeof args[args.length - 1] === 'function' ) {
+         args[args.length - 1] = wrapFunction(fnName + ' -> callback',
+         	args[args.length - 1]);
+         totalSize += args[args.length - 1].arguments[1];
+         coutCallBack++;
+      }
+
+      var start = process.hrtime();
+      fn.apply(undefined, args);
+      var end = process.hrtime(start);
+      var timeS = end[0] + (end[1] / 1000000);
+      avgTime = ((countCallFunction - 1)*avgTime + timeS)/countCallFunction;
     }
   }
+
+function wrapAll(api) {
+	var wraperedApi = cloneInterface(api);
+	for(var key in wraperedApi) {
+	   if(typeof wraperedApi[key] === 'function') {
+	       wraperedApi[key] = wrapFunction(key, wraperedApi[key]);
+	   }
+	}
+	return wraperedApi;
+}
+
+function printInformation(){
+	console.log('--------Iformation---------');
+	console.log('Calls: ' + countCallFunction);
+	console.log('CallBack: ' + coutCallBack);
+    console.log('Time: ' + avgTime + ' s');
+    console.log('---------------------------');
+}
